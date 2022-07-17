15973b085da8b23a16cf4059f36bad74f217bb75
unknown <jadentr.44@gmail.com>
2022-07-17 16:13:51 -0500

    can ask questions

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer
+  .prompt([{name:"title",message:"What is the name of your project?"},
+{name:"description",message:"Give me a description of your project"}])
+.then((resposes) =>{
+  console.log(resposes)
+})
+
+
