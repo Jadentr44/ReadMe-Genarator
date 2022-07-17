535b04f87377bc278670afd6c8334f7d605c3003
unknown <jadentr.44@gmail.com>
2022-07-17 17:10:34 -0500

    added questions tab

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 
-<h1 id="top" align="center">jaden</h1>
+<h1 id="top" align="center">Github README genarator</h1>
 
 ## Description
 ![Optinal Image here](path/to/file)
 
-description
+well its prestty self explanatory, you enter the details of your read me and it makes you a profesanoal readme
 <p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+## Questions
+[Github](https://github.com/Jadentr44)
+email:jadentr.44@gmail.com
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { functions } = require('lodash');
 
 inquirer
   .prompt([{name:"title",message:"What is the name of your project?"},
-{name:"description",message:"Give me a description of your project"}])
+{name:"description",message:"Give me a description of your project"},
+{name:"githubUserName",message:"Enter your github username"},
+{name:"email",message:"Enter your email"}])
 .then((resposes) =>{
   fs.writeFile('README.md', createReadMe(resposes), (err) =>
   err ? console.error(err) : console.log('Check your new read me!')
@@ -26,5 +28,14 @@ return`
 
 ${response.description}
 <p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+## Questions
+[Github](https://github.com/${response.githubUserName})
+
+email:${response.email}
+
+Contact me with any questions
 `
 }
