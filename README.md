21a731687e7481f5ea0032b60f3b8949f0febaa5
unknown <jadentr.44@gmail.com>
2022-07-17 16:37:17 -0500

    responses will be put in basic README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ReadMe-maker
+
+<h1 id="top" align="center">jaden</h1>
+
+## Description
+![Optinal Image here](path/to/file)
+description
+<p align="right">(<a href="#top">back to top</a>)</p>
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 //  imports i need
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { functions } = require('lodash');
 
 inquirer
   .prompt([{name:"title",message:"What is the name of your project?"},
 {name:"description",message:"Give me a description of your project"}])
 .then((resposes) =>{
-  console.log(resposes)
+  fs.writeFile('README.md', createReadMe(resposes), (err) =>
+  err ? console.error(err) : console.log('Check your new read me!')
+);
 })
 
 
+function createReadMe(responseObj){
+
+return`
+<h1 id="top" align="center">${responseObj.title}</h1>
+
+## Description
+![Optinal Image here](path/to/file)
+
+${responseObj.description}
+<p align="right">(<a href="#top">back to top</a>)</p>
+`
+}
